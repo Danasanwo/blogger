ea8747ac7bc04393f26b7e3ae9d19e0284a0bc03
Danasanwo <Danasanwo@gmail.com>
2020-06-14 20:09:07 +0100

    nav added

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,72 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <div id="home">
+        <router-link to="/">Hieroglyphics</router-link>
+      </div>
+      <div>
+        <router-link to="/follow">Follow</router-link>
+      </div>
+
+      <div>
+        <router-link to="/write">Write</router-link>
+      </div>
+      <div>
+        <button>sign in</button>
+      </div>
+      <div>
+        <router-link to="/getstarted">
+          <button>get started</button>
+        </router-link>
+      </div>
     </div>
     <router-view />
   </div>
 </template>
 
-<style>
+<style lang="scss">
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
 
 #nav {
-  padding: 30px;
+  // width: 100%;
+  height: 8%;
+  padding: 10px;
+  display: flex;
+  align-items: center;
+  background: #f3e40f;
+  // box-shadow: 0px 5px 15px #cccccc;
+  #home {
+    // flex: 5;
+    width: 60vw;
+    min-width: 30vw;
+    font-size: 2rem;
+    font-family: Georgia, 'Times New Roman', Times, serif;
+  }
 }
 
 #nav a {
   font-weight: bold;
   color: #2c3e50;
+  text-decoration: none;
+  padding: 1rem;
+}
+
+button {
+  outline: none;
+  border: 2px solid #2c3e50;
+  width: 9.5rem;
+  height: 8vh;
 }
 
 #nav a.router-link-exact-active {
---
+++ src/views/Getstarted.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>money</h1>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>

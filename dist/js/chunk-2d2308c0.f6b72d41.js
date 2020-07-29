57704fff88dc73f2114fd922940cd7ee45e3f2ef
Danasanwo <63405209+Danasanwo@users.noreply.github.com>
2020-07-29 20:47:45 +0100

    Add files via upload

---
+++ dist/js/chunk-2d2308c0.f6b72d41.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d2308c0"],{ed6a:function(t,e,n){"use strict";n.r(e);var s=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},c=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"write"},[n("iframe",{attrs:{src:"https://strapic-cms-db.herokuapp.com/admin",title:"link to strapi cms"}})])}],r={},a=r,i=n("2877"),l=Object(i["a"])(a,s,c,!1,null,null,null);e["default"]=l.exports}}]);
+//# sourceMappingURL=chunk-2d2308c0.f6b72d41.js.map
